fe3d0e9ab098f7e94bc4ab8d876eeba9fc958c6b
Jeff Hiner <37913568+jeff-hiner@users.noreply.github.com>
2021-02-20 19:09:39 -0700

    Update dependencies and fix cross-compatibility issues (#24)
    * Update dependencies and fix some cross-compatibility issues
    * version 0.11.0-alpha.1

--- src/raw.rs
+++ src/raw.rs
@@ -1,14 +1,14 @@
-ioctl!(read eviocgeffects with b'E', 0x84; ::libc::c_int);
-ioctl!(read eviocgid with b'E', 0x02; /*struct*/ input_id);
-ioctl!(read eviocgkeycode with b'E', 0x04; [::libc::c_uint; 2]);
-ioctl!(read eviocgrep with b'E', 0x03; [::libc::c_uint; 2]);
-ioctl!(read eviocgversion with b'E', 0x01; ::libc::c_int);
-ioctl!(write_int eviocrmff with b'E', 0x81);
+ioctl_read!(eviocgeffects, b'E', 0x84, ::libc::c_int);
+ioctl_read!(eviocgid, b'E', 0x02, /*struct*/ input_id);
+ioctl_read!(eviocgkeycode, b'E', 0x04, [::libc::c_uint; 2]);
+ioctl_read!(eviocgrep, b'E', 0x03, [::libc::c_uint; 2]);
+ioctl_read!(eviocgversion, b'E', 0x01, ::libc::c_int);
+ioctl_write_int!(eviocrmff, b'E', 0x81);
 // ioctl!(read eviocgkeycode_v2 with b'E', 0x04; /*struct*/ input_keymap_entry);
 // TODO #define EVIOCSFF _IOC ( _IOC_WRITE , 'E' , 0x80 , sizeof ( struct ff_effect ) )
-ioctl!(write_ptr eviocskeycode with b'E', 0x04; [::libc::c_uint; 2]);
+ioctl_write_ptr!(eviocskeycode, b'E', 0x04, [::libc::c_uint; 2]);
 // ioctl!(write_int eviocskeycode_v2 with b'E', 0x04; /*struct*/ input_keymap_entry);
-ioctl!(write_ptr eviocsrep with b'E', 0x03; [::libc::c_uint; 2]);
+ioctl_write_ptr!(eviocsrep, b'E', 0x03, [::libc::c_uint; 2]);
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -191,38 +191,25 @@ impl ::std::default::Default for ff_rumble_effect {
     fn default() -> Self { unsafe { ::std::mem::zeroed() } }
 }
 
-ioctl!(read_buf eviocgname with b'E', 0x06; u8);
-ioctl!(read_buf eviocgphys with b'E', 0x07; u8);
-ioctl!(read_buf eviocguniq with b'E', 0x08; u8);
-ioctl!(read_buf eviocgprop with b'E', 0x09; u8);
-ioctl!(read_buf eviocgmtslots with b'E', 0x0a; u8);
-ioctl!(read_buf eviocgkey with b'E', 0x18; u8);
-ioctl!(read_buf eviocgled with b'E', 0x19; u8);
-ioctl!(read_buf eviocgsnd with b'E', 0x1a; u8);
-ioctl!(read_buf eviocgsw with b'E', 0x1b; u8);
+ioctl_read_buf!(eviocgname, b'E', 0x06, u8);
+ioctl_read_buf!(eviocgphys, b'E', 0x07, u8);
+ioctl_read_buf!(eviocguniq, b'E', 0x08, u8);
+ioctl_read_buf!(eviocgprop, b'E', 0x09, u8);
+ioctl_read_buf!(eviocgmtslots, b'E', 0x0a, u8);
+ioctl_read_buf!(eviocgkey, b'E', 0x18, u8);
+ioctl_read_buf!(eviocgled, b'E', 0x19, u8);
+ioctl_read_buf!(eviocgsnd, b'E', 0x1a, u8);
+ioctl_read_buf!(eviocgsw, b'E', 0x1b, u8);
 
-ioctl!(write_ptr eviocsff with b'E', 0x80; ff_effect);
-ioctl!(write_int eviocgrab with b'E', 0x90);
-ioctl!(write_int eviocrevoke with b'E', 0x91);
-ioctl!(write_int eviocsclockid with b'E', 0xa0);
+ioctl_write_ptr!(eviocsff, b'E', 0x80, ff_effect);
+ioctl_write_int!(eviocgrab, b'E', 0x90);
+ioctl_write_int!(eviocrevoke, b'E', 0x91);
+ioctl_write_int!(eviocsclockid, b'E', 0xa0);
 
-#[cfg(target_arch = "arm")]
 pub unsafe fn eviocgbit(fd: ::libc::c_int, ev: u32, len: ::libc::c_int, buf: *mut u8) -> ::nix::Result<i32> {
-    convert_ioctl_res!(::nix::libc::ioctl(fd, ior!(b'E', 0x20 + ev, len) as ::libc::c_long, buf))
+    convert_ioctl_res!(::nix::libc::ioctl(fd, request_code_read!(b'E', 0x20 + ev, len), buf))
 }
 
-#[cfg(not(target_arch = "arm"))]
-pub unsafe fn eviocgbit(fd: ::libc::c_int, ev: u32, len: ::libc::c_int, buf: *mut u8) -> ::nix::Result<i32> {
-    convert_ioctl_res!(::nix::libc::ioctl(fd, ior!(b'E', 0x20 + ev, len) as ::libc::c_ulong, buf))
-}
-
-#[cfg(target_arch = "arm")]
 pub unsafe fn eviocgabs(fd: ::libc::c_int, abs: u32, buf: *mut input_absinfo) -> ::nix::Result<i32> {
-    convert_ioctl_res!(::nix::libc::ioctl(fd, ior!(b'E', 0x40 + abs, ::std::mem::size_of::<input_absinfo>()) as ::libc::c_long, buf))
+    convert_ioctl_res!(::nix::libc::ioctl(fd, request_code_read!(b'E', 0x40 + abs, ::std::mem::size_of::<input_absinfo>()), buf))
 }
-
-#[cfg(not(target_arch = "arm"))]
-pub unsafe fn eviocgabs(fd: ::libc::c_int, abs: u32, buf: *mut input_absinfo) -> ::nix::Result<i32> {
-    convert_ioctl_res!(::nix::libc::ioctl(fd, ior!(b'E', 0x40 + abs, ::std::mem::size_of::<input_absinfo>()) as ::libc::c_ulong, buf))
-}
-
