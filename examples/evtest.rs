8c0ac27c1c56848c2f8e8fa04e190a173a79e070
Noah <33094578+coolreader18@users.noreply.github.com>
2021-02-22 21:09:31 -0600

    Make enumerate() return an iterator instead of a vec

--- examples/evtest.rs
+++ examples/evtest.rs
@@ -1,16 +1,13 @@
 // Similar to the evtest tool.
 
-extern crate evdev;
-
 use std::io::prelude::*;
 
 fn main() {
     let mut args = std::env::args_os();
-    let mut d;
-    if args.len() > 1 {
-        d = evdev::Device::open(&args.nth(1).unwrap()).unwrap();
+    let mut d = if args.len() > 1 {
+        evdev::Device::open(&args.nth(1).unwrap()).unwrap()
     } else {
-        let mut devices = evdev::enumerate();
+        let mut devices = evdev::enumerate().collect::<Vec<_>>();
         for (i, d) in devices.iter().enumerate() {
             println!("{}: {:?}", i, d.name());
         }
@@ -18,8 +15,8 @@ fn main() {
         let _ = std::io::stdout().flush();
         let mut chosen = String::new();
         std::io::stdin().read_line(&mut chosen).unwrap();
-        d = devices.swap_remove(chosen.trim().parse::<usize>().unwrap());
-    }
+        devices.swap_remove(chosen.trim().parse::<usize>().unwrap())
+    };
     println!("{}", d);
     println!("Events:");
     loop {
