eb84ea0e296f826d9c43a63ac028c7d08361bd48
Jonas Bosse <jonas.bosse@posteo.de>
2023-01-21 17:23:05 +0100

    update virtual_joystick

--- examples/virtual_joystick.rs
+++ examples/virtual_joystick.rs
@@ -2,14 +2,14 @@
 // Generally this requires root.
 
 use evdev::{
-    uinput::VirtualDeviceBuilder, AbsInfo, AbsoluteAxisType, EventType, InputEvent, UinputAbsSetup,
+    uinput::VirtualDeviceBuilder, AbsInfo, AbsAxisType, UinputAbsSetup, AbsAxisEvent,
 };
 use std::thread::sleep;
 use std::time::Duration;
 
 fn main() -> std::io::Result<()> {
     let abs_setup = AbsInfo::new(256, 0, 512, 20, 20, 1);
-    let abs_x = UinputAbsSetup::new(AbsoluteAxisType::ABS_X, abs_setup);
+    let abs_x = UinputAbsSetup::new(AbsAxisType::ABS_X, abs_setup);
 
     let mut device = VirtualDeviceBuilder::new()?
         .name("Fake Joystick")
@@ -22,18 +22,17 @@ fn main() -> std::io::Result<()> {
         println!("Available as {}", path.display());
     }
 
-    let type_ = EventType::ABSOLUTE;
     // Hopefully you don't have ABS_X bound to anything important.
-    let code = AbsoluteAxisType::ABS_X.0;
+    let code = AbsAxisType::ABS_X.0;
 
     println!("Waiting for Ctrl-C...");
     loop {
-        let down_event = InputEvent::new(type_, code, 0);
+        let down_event = AbsAxisEvent::new( code, 0);
         device.emit(&[down_event]).unwrap();
         println!("Minned out.");
         sleep(Duration::from_secs(2));
 
-        let up_event = InputEvent::new(type_, code, 512);
+        let up_event = AbsAxisEvent::new(code, 512);
         device.emit(&[up_event]).unwrap();
         println!("Maxed out.");
         sleep(Duration::from_secs(2));
